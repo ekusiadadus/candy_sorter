bdcddcfa4d994c12ccd444d87cec68819127c8fc
Deam <deam@aleph.engineering>
2022-03-14 18:45:36 -0400

    space and register bowls as drag target

--- lib/features/candy_sorter/view/bowl_area.dart
+++ lib/features/candy_sorter/view/bowl_area.dart
@@ -6,20 +6,35 @@ class BowlArea extends StatelessWidget {
   const BowlArea({
     Key? key,
     required this.game,
+    required this.constraints,
   }) : super(key: key);
 
   final Game game;
+  final BoxConstraints constraints;
 
   @override
   Widget build(BuildContext context) {
+    final width = constraints.maxWidth / game.colors.length;
     return Wrap(
-      alignment: WrapAlignment.spaceEvenly,
-      runAlignment: WrapAlignment.spaceEvenly,
+      alignment: WrapAlignment.center,
+      runSpacing: 5.0,
+      spacing: 5.0,
+      runAlignment: WrapAlignment.center,
       children: [
         for (var i = 0; i < game.colors.length; i++)
-          Bowl(
-            color: game.colors[i],
-          )
+          DragTarget<Candy>(
+            builder: (BuildContext context, List<dynamic> accepted,
+                List<dynamic> rejected) {
+              return Bowl(
+                color: game.colors[i],
+                width: width,
+              );
+            },
+            onWillAccept: (Candy? data) {
+              print('onWillAccept: ${data?.color == game.colors[i]}');
+              return data?.color == game.colors[i];
+            },
+          ),
       ],
     );
   }
