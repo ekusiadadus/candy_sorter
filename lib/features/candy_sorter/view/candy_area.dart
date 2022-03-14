3b87f2a1c7bffb57cc8418c169b0e8d03eab1b31
Deam <deam@aleph.engineering>
2022-03-14 18:44:15 -0400

    set animated draggable candies

--- lib/features/candy_sorter/view/candy_area.dart
+++ lib/features/candy_sorter/view/candy_area.dart
@@ -1,30 +1,84 @@
 import 'package:candy_sorter/features/candy_sorter/model/model.dart';
-import 'package:candy_sorter/features/candy_sorter/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
-class CandyArea extends StatelessWidget {
+import '../widgets/candy_widget.dart';
+
+class CandyArea extends StatefulWidget {
   const CandyArea({
     Key? key,
     required this.game,
+    required this.onRemoveCandy,
+    required this.onAddTwoLeft,
   }) : super(key: key);
 
   final Game game;
+  final Function onRemoveCandy;
+  final Function onAddTwoLeft;
+
+  @override
+  State<CandyArea> createState() => _CandyAreaState();
+}
+
+class _CandyAreaState extends State<CandyArea> {
+  Offset _beginCandyOffset = const Offset(0, 0);
+
+  _buildStack(BoxConstraints constraints) {
+    List<Widget> list = [];
+    for (var candy in widget.game.candies) {
+      list.add(
+        AnimatedPositioned(
+          top: candy.top,
+          left: candy.left,
+          curve: Curves.easeOut,
+          duration: const Duration(milliseconds: 250),
+          child: Draggable<Candy>(
+            data: candy,
+            feedback: CandyWidget(
+              candy: candy,
+            ),
+            child: CandyWidget(
+              candy: candy,
+            ),
+            childWhenDragging: Container(),
+            onDragCompleted: () {
+              widget.onRemoveCandy(candy);
+            },
+            onDragStarted: () {
+              setState(() {
+                _beginCandyOffset = Offset(candy.left, candy.top);
+              });
+            },
+            onDragUpdate: (details) {
+              setState(() {
+                candy.left = details.localPosition.dx;
+                candy.top = details.localPosition.dy - 120;
+              });
+            },
+            onDragEnd: (DraggableDetails details) {
+              if (!details.wasAccepted) {
+                widget.onAddTwoLeft();
+                widget.game.addTwo();
+                setState(() {
+                  candy.left = _beginCandyOffset.dx;
+                  candy.top = _beginCandyOffset.dy;
+                });
+              }
+              print('onDragEnd - accepted: ${details.wasAccepted}');
+            },
+          ),
+        ),
+      );
+    }
+    return list;
+  }
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constrains) {
         return Stack(
-          children: [
-            for (var candy in game.candies)
-              Positioned(
-                top: candy.top,
-                left: candy.left,
-                child: CandyWidget(
-                  candy: candy,
-                ),
-              ),
-          ],
+          clipBehavior: Clip.none,
+          children: _buildStack(constrains),
         );
       },
     );
