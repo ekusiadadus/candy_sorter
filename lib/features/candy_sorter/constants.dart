1d83a9945b485c6294723e39a645e29b9292e533
Deam <deam@aleph.engineering>
2022-03-15 17:32:51 -0400

    add candy width constant and use it

--- lib/features/candy_sorter/constants.dart
+++ lib/features/candy_sorter/constants.dart
@@ -1,2 +1,3 @@
 const double cMaxAppBarHeight = 120.0;
 const double cMaxCandiesCount = 200.0;
+const double cCandyWidth = 30.0;
--- lib/features/candy_sorter/widgets/candy_widget.dart
+++ lib/features/candy_sorter/widgets/candy_widget.dart
@@ -2,11 +2,13 @@ import 'package:candy_sorter/features/candy_sorter/model/model.dart';
 import 'package:candy_sorter/features/candy_sorter/widgets/colored_svg.dart';
 import 'package:flutter/material.dart';
 
+import '../constants.dart';
+
 class CandyWidget extends StatelessWidget {
   const CandyWidget({
     Key? key,
     required this.candy,
-    this.width = 30,
+    this.width = cCandyWidth,
   }) : super(key: key);
 
   final Candy candy;
