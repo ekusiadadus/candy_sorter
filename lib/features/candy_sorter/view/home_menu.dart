22e704103d8b02dbff2fb0dc722266b9f13f05df
Deam <deam@aleph.engineering>
2022-03-15 18:29:36 -0400

    refactor web view

--- lib/features/candy_sorter/view/home_menu.dart
+++ lib/features/candy_sorter/view/home_menu.dart
@@ -14,113 +14,166 @@ class _HomeMenuState extends State<HomeMenu> {
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     return SafeArea(
-      child: ListView(
-        padding: const EdgeInsets.all(40.0),
-        children: [
-          const SizedBox(
-            height: 50.0,
-          ),
-          Center(
-            child: Lottie.asset(
-              'assets/candies.json',
-              width: width / 2,
-              fit: BoxFit.fill,
-            ),
-          ),
-          Center(
-            child: Text(
-              "Candy Sorter",
-              style: TextStyle(color: Colors.black54, fontSize: (width * 0.1)),
-            ),
-          ),
-          const SizedBox(
-            height: 50.0,
-          ),
-          Center(
-            child: SizedBox(
-              width: width / 2,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    PageRouteBuilder(
-                      pageBuilder: (context, animation, secondaryAnimation) =>
-                          const GamePage(),
-                      transitionsBuilder:
-                          (context, animation, secondaryAnimation, child) {
-                        const begin = Offset(0.0, 1.0);
-                        const end = Offset.zero;
-                        const curve = Curves.easeInOut;
+      child: width <= 425
+          ? ListView(
+              padding: const EdgeInsets.all(40.0),
+              children: [
+                const SizedBox(
+                  height: 50.0,
+                ),
+                Center(
+                  child: Lottie.asset(
+                    'assets/candies.json',
+                    width: width / 2,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    "Candy Sorter",
+                    style: TextStyle(
+                        color: Colors.black54, fontSize: (width * 0.1)),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50.0,
+                ),
+                Center(
+                  child: SizedBox(
+                    width: width / 2,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          PageRouteBuilder(
+                            pageBuilder:
+                                (context, animation, secondaryAnimation) =>
+                                    const GamePage(),
+                            transitionsBuilder: (context, animation,
+                                secondaryAnimation, child) {
+                              const begin = Offset(0.0, 1.0);
+                              const end = Offset.zero;
+                              const curve = Curves.easeInOut;
 
-                        var tween = Tween(begin: begin, end: end)
-                            .chain(CurveTween(curve: curve));
-                        final curvedAnimation = CurvedAnimation(
-                          parent: animation,
-                          curve: curve,
-                        );
-                        return SlideTransition(
-                          position: tween.animate(curvedAnimation),
-                          child: child,
+                              var tween = Tween(begin: begin, end: end)
+                                  .chain(CurveTween(curve: curve));
+                              final curvedAnimation = CurvedAnimation(
+                                parent: animation,
+                                curve: curve,
+                              );
+                              return SlideTransition(
+                                position: tween.animate(curvedAnimation),
+                                child: child,
+                              );
+                            },
+                          ),
                         );
                       },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blue.shade500,
+                        elevation: 5.0,
+                        shadowColor: Colors.blue.shade300,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 50, vertical: 20),
+                      ),
+                      child: const FittedBox(
+                        fit: BoxFit.fill,
+                        child: Text(
+                          'New Game',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24.0,
+                              letterSpacing: 2.5,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
                     ),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.blue.shade500,
-                  elevation: 5.0,
-                  shadowColor: Colors.blue.shade300,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50),
                   ),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
                 ),
-                child: const FittedBox(
-                  fit: BoxFit.fill,
-                  child: Text(
-                    'New Game',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24.0,
-                        letterSpacing: 2.5,
-                        fontWeight: FontWeight.bold),
-                  ),
+                const SizedBox(
+                  height: 30.0,
+                ),
+              ],
+            )
+          : Center(
+              child: SizedBox(
+                width: width / 1.5,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Lottie.asset(
+                          'assets/candies.json',
+                          width: width / 3,
+                          fit: BoxFit.fill,
+                        )
+                      ],
+                    ),
+                    Center(
+                      child: SizedBox(
+                        width: width / 4,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              PageRouteBuilder(
+                                pageBuilder:
+                                    (context, animation, secondaryAnimation) =>
+                                        const GamePage(),
+                                transitionsBuilder: (context, animation,
+                                    secondaryAnimation, child) {
+                                  const begin = Offset(0.0, 1.0);
+                                  const end = Offset.zero;
+                                  const curve = Curves.easeInOut;
+
+                                  var tween = Tween(begin: begin, end: end)
+                                      .chain(CurveTween(curve: curve));
+                                  final curvedAnimation = CurvedAnimation(
+                                    parent: animation,
+                                    curve: curve,
+                                  );
+                                  return SlideTransition(
+                                    position: tween.animate(curvedAnimation),
+                                    child: child,
+                                  );
+                                },
+                              ),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.blue.shade500,
+                            elevation: 5.0,
+                            shadowColor: Colors.blue.shade300,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 50, vertical: 20),
+                          ),
+                          child: const FittedBox(
+                            fit: BoxFit.fill,
+                            child: Text(
+                              'New Game',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 24.0,
+                                  letterSpacing: 2.5,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 30.0,
-          ),
-          // Center(
-          //   child: SizedBox(
-          //     width: width / 2,
-          //     child: ElevatedButton(
-          //       onPressed: () {},
-          //       style: ElevatedButton.styleFrom(
-          //         primary: Colors.blue.shade500,
-          //         elevation: 5.0,
-          //         shadowColor: Colors.blue.shade300,
-          //         shape: RoundedRectangleBorder(
-          //           borderRadius: BorderRadius.circular(50),
-          //         ),
-          //         padding:
-          //             const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-          //       ),
-          //       child: const Text(
-          //         'New Game',
-          //         style: TextStyle(
-          //             color: Colors.white,
-          //             fontSize: 24.0,
-          //             letterSpacing: 2.5,
-          //             fontWeight: FontWeight.bold),
-          //       ),
-          //     ),
-          //   ),
-          // ),
-        ],
-      ),
     );
   }
 }
