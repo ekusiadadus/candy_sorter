84a79620ad52dcaa5f750a131bac560728eed4a3
Deam <deam@aleph.engineering>
2022-03-18 19:52:21 -0400

    animate the bowl on hover

--- lib/features/candy_sorter/view/bowl_area.dart
+++ lib/features/candy_sorter/view/bowl_area.dart
@@ -3,7 +3,7 @@ import 'package:candy_sorter/features/candy_sorter/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class BowlArea extends StatelessWidget {
-  const BowlArea({
+  BowlArea({
     Key? key,
     required this.game,
     required this.constraints,
@@ -11,6 +11,8 @@ class BowlArea extends StatelessWidget {
 
   final Game game;
   final BoxConstraints constraints;
+  late bool onHover = false;
+  late bool onAccepted = false;
 
   @override
   Widget build(BuildContext context) {
@@ -25,10 +27,18 @@ class BowlArea extends StatelessWidget {
           DragTarget<Candy>(
             builder: (BuildContext context, List<dynamic> accepted,
                 List<dynamic> rejected) {
-              return Bowl(
-                color: game.colors[i],
-                width: width,
+              onHover = accepted.isNotEmpty || rejected.isNotEmpty;
+              return AnimatedScale(
+                scale: onHover ? 1.1 : 1,
+                duration: const Duration(milliseconds: 250),
+                child: Bowl(
+                  color: game.colors[i],
+                  width: width,
+                ),
               );
+            },
+            onLeave: (Candy? candy) {
+              onHover = false;
             },
             onWillAccept: (Candy? data) {
               print('onWillAccept: ${data?.color == game.colors[i]}');
